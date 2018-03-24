720157a3c835024459984bdb02b6b81c2a582741
shiva322 <shivapadma322@gmail.com>
2018-03-24 15:08:40 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/group7/mortgage_calculator_lab2/CalculationFragment.java
@@ -0,0 +1,147 @@
+package com.example.group7.mortgage_calculator_lab2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CalculationFragment extends Fragment {
+    public View calcView;
+    public double radioTerms_value, monthly_amt;
+
+    public CalculationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        calcView = inflater.inflate(R.layout.fragment_calculation, container, false);
+
+        final Spinner spinner = (Spinner) calcView.findViewById(R.id.propType_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.proptype_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        final Spinner spinner1 = (Spinner) calcView.findViewById(R.id.state_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.state_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner1.setAdapter(adapter1);
+
+
+        final Button button_new = (Button) calcView.findViewById(R.id.button_new);
+        button_new.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                final EditText et_street = (EditText) calcView.findViewById(R.id.street);
+                final EditText et_city = (EditText) calcView.findViewById(R.id.city);
+                final EditText et_zipcode = (EditText) calcView.findViewById(R.id.zipcode);
+                final EditText et_propPrice = (EditText) calcView.findViewById(R.id.propPrice);
+                final EditText et_downpmt = (EditText) calcView.findViewById(R.id.downPmt);
+                final EditText et_apr = (EditText) calcView.findViewById(R.id.apr);
+                final TextView tv_monthly = (TextView) calcView.findViewById(R.id.monthly);
+                et_street.setText("");
+                et_city.setText("");
+                et_zipcode.setText("");
+                et_propPrice.setText("");
+                et_downpmt.setText("");
+                et_apr.setText("");
+               // spinner.setSelection(0);
+               // spinner1.setSelection(0);
+                tv_monthly.setText("");
+                RadioGroup radioGroup_terms = (RadioGroup) calcView.findViewById(R.id.rg_terms);
+                radioGroup_terms.clearCheck();
+
+
+            }
+        });
+
+
+        final Button button_save = (Button) calcView.findViewById(R.id.button_save);
+        button_save.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+        final Button button_calc = (Button) calcView.findViewById(R.id.button_calc);
+        button_calc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button Calc
+
+
+                final EditText et_propPrice = (EditText) calcView.findViewById(R.id.propPrice);
+                final EditText et_downpmt = (EditText) calcView.findViewById(R.id.downPmt);
+                final EditText et_apr = (EditText) calcView.findViewById(R.id.apr);
+                final TextView tv_monthly = (TextView) calcView.findViewById(R.id.monthly);
+
+                double propPrice = Double.parseDouble(et_propPrice.getText().toString());
+                double dwnPmt = Double.parseDouble(et_downpmt.getText().toString());
+                double apr = Double.parseDouble(et_apr.getText().toString());
+                apr = apr / 100;
+                radioTerms_value = radioTerms_value * 12;
+                Log.v("apr", apr + "");
+                Log.v("xxx", Math.pow(1 + apr, radioTerms_value) + " ");
+
+                Log.v("thly", radioTerms_value + "");
+
+                double loan_amt = propPrice - dwnPmt;
+
+                monthly_amt = (loan_amt) * ((apr * (Math.pow(1 + apr, radioTerms_value))) / (Math.pow(1 + apr, radioTerms_value) - 1));
+                Log.v("monthly", monthly_amt + "");
+                tv_monthly.setText(monthly_amt + "");
+
+
+            }
+        });
+
+
+        // Inflate the layout for this fragment
+        return calcView;
+
+    }
+
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.radio_fifteen:
+                if (checked)
+                    // Pirates are the best
+                    radioTerms_value = 15;
+                break;
+            case R.id.radio_thirty:
+                if (checked)
+                    radioTerms_value = 30;
+                break;
+        }
+
+    }
+
+}
